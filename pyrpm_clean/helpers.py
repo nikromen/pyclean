4993ac154a94225bf2c6693262866a9a6d7c245f
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2024-10-16 12:49:52 +0200

    add full package name to table

--- pyrpm_clean/helpers.py
+++ pyrpm_clean/helpers.py
@@ -7,22 +7,23 @@ if TYPE_CHECKING:
 
 
 def dupe_table(
-    pkg_name: str,
+    name: str,
     package_dupes: list[PackageInfo],
     verbose: bool = False,
 ) -> str:
     """
     Create a table with duplicite packages.
     """
-    table_delimiter = "  | " + "-" * 103 + " |"
-    main_delimiter = "=" * 109
+    table_delimiter = "  | " + "-" * 124 + " |"
+    main_delimiter = "=" * 130
     table = [
         main_delimiter,
         "",
-        f"Package: {pkg_name}",
+        f"Name: {name}",
         "Duplicities found:",
-        "  | {:<55} {:<15} {:<15} {:<15} |".format(
+        "  | {:<55} {:<20} {:<15} {:<15} {:<15} |".format(
             "Location",
+            "Package full name",
             "Version",
             "Installer",
             "Files count",
@@ -32,15 +33,15 @@ def dupe_table(
     for dupe in package_dupes:
         installer_type = dupe.pkg_type.name if dupe.pkg_type else "unknown"
         table.append(
-            f"  | {dupe.location: <55} {dupe.version: <15} {installer_type: <15} "
-            f"{len(dupe.files): <15} |",
+            f"  | {dupe.location: <55} {dupe.package_name: <20} {dupe.version: <15} "
+            f"{installer_type: <15} {len(dupe.files): <15} |",
         )
         if not verbose:
             continue
 
-        table.append("  | Files:" + " " * 97 + " |")
+        table.append("  | Files:" + " " * 113 + " |")
         for file in dupe.files:
-            table.append(f"  |   {file:<101} |")
+            table.append(f"  |   {file:<117} |")
 
         table.append(table_delimiter)
 
